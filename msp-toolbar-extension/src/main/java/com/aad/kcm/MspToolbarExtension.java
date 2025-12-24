7b0f04a31e5f1a0ed89a599fc05295db28347f34
Paul Deelen <p.deelen@hotmail.com>
2025-12-24 17:00:22 +0100

    update

---
+++ msp-toolbar-extension/src/main/java/com/aad/kcm/MspToolbarExtension.java
@@ -0,0 +1,10 @@
+package com.aad.kcm;
+
+import org.apache.guacamole.extension.GuacamoleExtension;
+
+public class MspToolbarExtension extends GuacamoleExtension {
+    @Override
+    public String getName() {
+        return "msp-toolbar";
+    }
+}
