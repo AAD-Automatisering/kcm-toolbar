2f9e2e18acd298e25ff27caf835246dbeec707b8
Paul Deelen <p.deelen@hotmail.com>
2025-12-28 12:20:37 +0100

    fix button

--- msp-toolbar-extension/js/toolbar.js
+++ msp-toolbar-extension/js/toolbar.js
@@ -18,12 +18,93 @@
     document.body.prepend(toolbar);
   };
 
+  const getMenuElement = () =>
+    document.querySelector(".guac-menu.menu") || document.querySelector(".guac-menu");
+
+  const clickMenuToggle = (root) => {
+    const toggle = root?.querySelector?.(".menu-toggle, .menu-button, .toggle") ||
+      document.querySelector(".menu-toggle, .menu-button, .toggle");
+    if (toggle) {
+      toggle.click();
+      return true;
+    }
+    return false;
+  };
+
+  const toggleScope = (scope) => {
+    if (!scope) {
+      return false;
+    }
+
+    const functionKeys = [
+      "toggleMenu",
+      "toggleMenuVisible",
+      "toggleMenuShown",
+      "toggleMenuOpen",
+      "toggleSidebar",
+      "toggleSideMenu"
+    ];
+    for (const key of functionKeys) {
+      if (typeof scope[key] === "function") {
+        scope[key]();
+        scope.$applyAsync?.();
+        return true;
+      }
+    }
+
+    const booleanKeys = [
+      "menuVisible",
+      "menuOpen",
+      "menuShown",
+      "showMenu",
+      "isMenuVisible",
+      "sidebarVisible",
+      "sidebarOpen",
+      "sideMenuVisible"
+    ];
+    for (const key of booleanKeys) {
+      if (typeof scope[key] === "boolean") {
+        scope[key] = !scope[key];
+        scope.$applyAsync?.();
+        return true;
+      }
+    }
+
+    return toggleScope(scope.$parent);
+  };
+
   const toggleMenu = () => {
-    const menu = document.querySelector(".guac-menu.menu");
+    const menu = getMenuElement();
     if (!menu) {
       return;
     }
-    menu.classList.toggle("open");
+
+    if (clickMenuToggle(menu)) {
+      return;
+    }
+
+    if (window.angular && angular.element) {
+      const scope = angular.element(menu).scope();
+      if (toggleScope(scope)) {
+        return;
+      }
+    }
+
+    const rect = menu.getBoundingClientRect();
+    const width = rect.width || menu.offsetWidth || 0;
+    const isHidden = rect.right <= 0 || rect.left < -10;
+    if (isHidden) {
+      menu.classList.add("open");
+      menu.style.display = "block";
+      menu.style.left = "0px";
+      menu.style.transform = "translateX(0px)";
+    } else {
+      menu.classList.remove("open");
+      if (width) {
+        menu.style.left = `-${width}px`;
+        menu.style.transform = `translateX(-${width}px)`;
+      }
+    }
   };
 
   const init = () => {
