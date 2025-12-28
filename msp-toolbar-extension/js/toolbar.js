386d3a64199cc0a516cddae681ee28f781aa28ab
Paul Deelen <p.deelen@hotmail.com>
2025-12-28 12:25:56 +0100

    remove fallback

--- msp-toolbar-extension/js/toolbar.js
+++ msp-toolbar-extension/js/toolbar.js
@@ -18,9 +18,6 @@
     document.body.prepend(toolbar);
   };
 
-  const getMenuElement = () =>
-    document.querySelector(".guac-menu.menu") || document.querySelector(".guac-menu");
-
   const dispatchKey = (type, key, code, keyCode, modifiers) => {
     const event = new KeyboardEvent(type, {
       key,
@@ -57,114 +54,8 @@
     dispatchKey("keyup", "Control", "ControlLeft", 17, { ctrlKey: false });
   };
 
-  const clickMenuToggle = (root) => {
-    const toggle = root?.querySelector?.(".menu-toggle, .menu-button, .toggle") ||
-      document.querySelector(".menu-toggle, .menu-button, .toggle");
-    if (toggle) {
-      toggle.click();
-      return true;
-    }
-    return false;
-  };
-
-  const toggleScope = (scope) => {
-    if (!scope) {
-      return false;
-    }
-
-    const functionKeys = [
-      "toggleMenu",
-      "toggleMenuVisible",
-      "toggleMenuShown",
-      "toggleMenuOpen",
-      "toggleSidebar",
-      "toggleSideMenu"
-    ];
-    for (const key of functionKeys) {
-      if (typeof scope[key] === "function") {
-        scope[key]();
-        scope.$applyAsync?.();
-        return true;
-      }
-    }
-
-    const booleanKeys = [
-      "menuVisible",
-      "menuOpen",
-      "menuShown",
-      "showMenu",
-      "isMenuVisible",
-      "sidebarVisible",
-      "sidebarOpen",
-      "sideMenuVisible"
-    ];
-    for (const key of booleanKeys) {
-      if (typeof scope[key] === "boolean") {
-        scope[key] = !scope[key];
-        scope.$applyAsync?.();
-        return true;
-      }
-    }
-
-    return toggleScope(scope.$parent);
-  };
-
-  const isMenuVisible = (menu) => {
-    const rect = menu.getBoundingClientRect();
-    return rect.width > 0 && rect.right > 10 && rect.left >= -10;
-  };
-
-  const showMenu = (menu) => {
-    menu.classList.add("open");
-    menu.style.display = "block";
-    menu.style.left = "0px";
-    menu.style.transform = "translateX(0px)";
-  };
-
-  const hideMenu = (menu) => {
-    menu.classList.remove("open");
-    const rect = menu.getBoundingClientRect();
-    const width = rect.width || menu.offsetWidth || 0;
-    if (width) {
-      menu.style.left = `-${width}px`;
-      menu.style.transform = `translateX(-${width}px)`;
-    }
-  };
-
   const toggleMenu = () => {
-    const menu = getMenuElement();
-    const wasVisible = menu ? isMenuVisible(menu) : null;
-
     triggerMenuShortcut();
-
-    if (!menu) {
-      return;
-    }
-
-    if (clickMenuToggle(menu)) {
-      return;
-    }
-
-    if (window.angular && angular.element) {
-      const scope = angular.element(menu).scope();
-      if (toggleScope(scope)) {
-        return;
-      }
-    }
-
-    setTimeout(() => {
-      const nowVisible = isMenuVisible(menu);
-      if (wasVisible === null) {
-        return;
-      }
-      if (nowVisible === wasVisible) {
-        if (nowVisible) {
-          hideMenu(menu);
-        } else {
-          showMenu(menu);
-        }
-      }
-    }, 0);
   };
 
   const init = () => {
